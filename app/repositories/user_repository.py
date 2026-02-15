5919571d2dd491cfe82a679cb934b23a8550efb0
Saifur Rahman <saifurrahman@gmail.com>
2026-02-15 22:27:48 +0600

    feat: add user repository with soft delete awareness

---
+++ app/repositories/user_repository.py
@@ -0,0 +1,26 @@
+from sqlalchemy.orm import Session
+from app.models.user import User
+
+
+class UserRepository:
+    """
+    Handles ONLY database operations.
+    """
+
+    @staticmethod
+    def get_by_email(db: Session, email: str):
+        
+        return(
+            db.query(User).filter(
+                User.email == email,
+                User.is_active == True
+            ).first()
+        )
+    
+    @staticmethod
+    def create(db: Session, user: User):
+        db.add(user)
+        db.flush() # assign ID without commit
+        return user
+    
+    
